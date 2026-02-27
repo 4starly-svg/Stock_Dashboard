a9fde9734f4cacb4e317165836a45f83916b734a
4starly-svg <4starly@gmail.com>
2026-02-27 21:23:22 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,1 @@
-// This just contains all the api keys that this app uses!
-
-// THIS IS FOR 12data
-const API_KEY = "db575e6498614445b5b15465b45686dc";
-
-
-
-export { API_KEY }
+const API_KEY = "123abc456xyz";  // <-- this is your real Twelve Data key
